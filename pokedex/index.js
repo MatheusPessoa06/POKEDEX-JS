9bfc0e3a9fb0461a98e80377183d08ca68efd800
Matheus Pessoa <55144224+matheuspessoa2010@users.noreply.github.com>
2022-06-02 16:07:13 -0300

    Add files via upload

---
+++ pokedex/index.js
@@ -0,0 +1,32 @@
+const fetchPokemon = () =>{
+    //Salva no id o pokemon que aparecer na pag
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const pokemonPromises = []
+    //Percorre o id até 150, e pega os valores de cada pokemon
+    for(let i = 1; i <= 151; i++){
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))    
+    }
+
+    Promise.all(pokemonPromises)
+    .then(pokemons =>{
+        
+        const lisPokemons = pokemons.reduce((accumulator, pokemon) =>{
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+            
+            accumulator += 
+            `<li class="card">
+            <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+             <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+             <p class="card-subtitle">${types.join(' | ')}</p>
+            </li>`
+            return accumulator
+        }, '')
+
+        const ul = document.querySelector('[data-js="pokedex"]')
+
+        ul.innerHTML = lisPokemons
+    })
+}
+
+fetchPokemon()
